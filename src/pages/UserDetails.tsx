a66b50f0758d8470f2b5e7af7e358a8735cc9aa3
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-07-08 21:16:43 -0400

    create Example

---
+++ src/pages/UserDetails.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
+// import { Container } from './styles';
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserDetails: React.FC = () => {
+  const { id } = useParams();
+  const { data } = useFetch<User>(`users/${id}`);
+
+  if(!data) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <ul>
+      <li>ID: {data?.id}</li>
+      <li>Name: {data?.name}</li>
+    </ul>
+  );
+}
+
+export default UserDetails;
